90c085b7ccef9f1b9557145868043d2bd8e68408
Anjay <rajputanjay@gmail.com>
2024-06-29 22:43:57 +0530

    organise some line

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
+const router = Router()
+
 import { logoutUser, loginUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controllers.js"
 import { upload }  from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
-const router = Router()
 router.route("/register").post(
     upload.fields([
         {
